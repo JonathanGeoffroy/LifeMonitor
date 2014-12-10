92c5bd5ba33446705c49fe03f18553e16c6c7855
Philippon Romain <philippon@b10p5.fil.univ-lille1.fr>
2014-12-10 11:39:24 +0100

    Update dialog layout for genetic disease and dialog fragment

--- app/src/main/java/lifemonitor/application/controller/medicalRecord/widget/AddGeneticDeseaseDialog.java
+++ app/src/main/java/lifemonitor/application/controller/medicalRecord/widget/AddGeneticDeseaseDialog.java
@@ -5,6 +5,9 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import lifemonitor.application.R;
@@ -22,7 +25,22 @@ public class AddGeneticDeseaseDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(R.layout.dialog_add_genetic_desease);
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.dialog_add_genetic_desease, null);
+
+        builder.setView(dialogLayout);
+
+        Spinner spinner = (Spinner)dialogLayout.findViewById(R.id.spinner_genetic_list);
+
+        // TODO : Appends genetic disease in the spinner
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println("I'm here");
+            }
+        });
 
         // Create the AlertDialog object and return it
         return builder.create();
